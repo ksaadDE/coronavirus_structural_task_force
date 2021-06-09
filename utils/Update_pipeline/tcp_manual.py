d01d9e242f4e11f94deb8fad88954a3884c9ba04
KNolte19 <60223645+KNolte19@users.noreply.github.com>
2021-06-09 14:54:16 +0200

    added script to manually change proteins of entry

---
+++ utils/Update_pipeline/tcp_manual.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import pandas as pd
+"""
+script to manually assign proteins in non_assigned folder
+"""
+
+def main(pdb_id, protein):
+    repo_path = os.path.abspath(os.join(__file__ ,"../../..","pdb"))
+    move_n_del(repo_path, pdb_id, protein)
+
+def move_n_del(repo_path, pdb_id, protein):
+    shutil.copytree(os.path.join(repo_path,"not_assigned","SARS-CoV-2",pdb_id),os.path.join(repo_path,protein,"SARS-CoV-2",pdb_id))
+    shutil.rmtree(os.path.join(repo_path,"not_assigned","SARS-CoV-2",pdb_id))
+
+def change_database_entry(pdb_id, protein):
+    df = pd.read_pickle("main_repo_database.pkl")
+    df.loc[df["pdb_id"] == pdb_id, "protein"] = protein
+    df.to_pickle("main_repo_database.pkl")

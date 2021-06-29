aedfe75b2ba7b9b412b8fad4c69aefafc56c085f
MaximilianEdich <medich@physnet.uni-hamburg.de>
2021-06-29 13:46:51 +0200

    modified script to find now pdbs without haruspex results

--- utils/check_haruspex_results_exists.py
+++ utils/check_haruspex_results_exists.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+This script can be modified to detect any interesting information from a 
+README.md in the pdb folders.
+So far it only checks wether such a README.md exists and if so, it checks
+for the method == Cryo EM and if Haruspex results are available.
+
 Created on Mon Jun 28 17:04:23 2021
 
 @author: Maximilian Edich
 """
+
+look_method = "**Method**: Cryo-EM"
+look_info = "## HARUSPEX results\n\nNot available"
+
+
 
 import os
 
@@ -14,6 +24,10 @@ if os.path.basename(os.getcwd()) != "utils":
 
 
 pdb_dir = os.path.abspath(os.path.join(os.getcwd(), "..", "pdb"))
+
+# init list gathering useful information from readmes
+no_read_me_prots = []
+hits = []
 
 # iterate down the folder tree until correct README.md is found
 for protein in os.listdir(pdb_dir):
@@ -34,19 +48,35 @@ for protein in os.listdir(pdb_dir):
                 content = file.read()
                 file.close()
             except FileNotFoundError:
-                print("ERROR: README.md not found in:")
-                print(read_me_path)
-                print()
+                no_read_me_prots.append(os.path.join(protein,
+                                                     taxonomy, pdb_id))
                 continue
             
-            if content.find("**Method**: Cryo-EM") > -1:
-                # is cryo
-                print("Obtained from Cryo EM:")
-                print(read_me_path)
-                print("Haruspex available?")
-                print(content.find("## HARUSPEX results") > -1)
-                print()
-                
+            # check if method is the one you are looking for
+            if content.find(look_method) > -1:
+                if content.find(look_info) > -1:
+                    # open pdb file and look for additional info
+                    """pdb_path = os.path.join(taxonomy_dir, pdb_id, pdb_id + ".pdb")
+                    file = open (pdb_path, 'r')
+                    pdb_content = file.read()
+                    file.close()
+                    emd_number_start = pdb_content.find("EMD-")
+                    emd_number = pdb_content[emd_number_start : (emd_number_start + 10)].strip()
+                    """
+                    #hits.append(os.path.join(protein, taxonomy, pdb_id) + " | " + str(emd_number))
+                    hits.append(os.path.join(protein, taxonomy, pdb_id))
+
+print("Missing README.md files in:")
+print("length: " + str(len(no_read_me_prots)))
+for miss in no_read_me_prots:
+    print(miss)
+
+print()
+print("Structures obtained from '"
+      + look_method + "' and containing \n'" + look_info + "':")
+print("length: " + str(len(hits)))
+for hit in hits:
+    print(hit)
                 
             
     

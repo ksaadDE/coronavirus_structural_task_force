5994ddb8638869c7fc7d127e6983ffb45e4da315
MaximilianEdich <medich@physnet.uni-hamburg.de>
2021-10-11 12:19:52 +0200

    Domain classification tool updates

--- utils/protein_and_domain_classifier.py
+++ utils/protein_and_domain_classifier.py
@@ -8,6 +8,7 @@ and classifies each PDB to the corresponding domain.
 Classification of domains is based on a sequence alignment.
 You may have to create a fasta file first, which contains all sequences of
 the domains.
+At the end, an xcel sheet is given out, containing all relevant information.
 
 @author: localadmin
 """
@@ -17,6 +18,7 @@ from os.path import join
 import argparse
 import gemmi
 from Bio import SeqIO
+import xlsxwriter
 
 # handle input arguments
 parser = argparse.ArgumentParser(description='Run weekly update.')
@@ -26,6 +28,8 @@ args = parser.parse_args()
 
 taxo_sars_cov = "SARS-CoV"
 taxo_sars_cov_2 = "SARS-CoV-2"
+
+working_dir = os.getcwd()
 
 if args.taxonomy != taxo_sars_cov and args.taxonomy != taxo_sars_cov_2:
     exit("ERROR: Wrong taxonomy, check typos!")
@@ -38,7 +42,10 @@ print("work with files from:")
 print(os.getcwd())
 
 # load domain sequences from fasta file
-fasta_sequences = SeqIO.parse(open(join(os.getcwd(), "sequence_domain_info.fasta")),'fasta')
+try:
+    fasta_sequences = SeqIO.parse(open(join(os.getcwd(), "sequence_domain_info.fasta")),'fasta')
+except FileNotFoundError:
+    exit("ERROR: no file called 'sequence_domain_info.fasta' found in target folder.")
 fasta_seq_iter = []
 for fasta in fasta_sequences:
     domain_name, domain_sequence = fasta.id, str(fasta.seq)
@@ -47,26 +54,26 @@ for fasta in fasta_sequences:
 # lists for final results
 entries_and_domains = []
 
-doit = ['5rvv', '5rvp', '7nfv', '6wen', '5rvm', '5rvt', '7kxb', '6wey', '5rvu', '7kqw', '6wrh', '7kg3', '5rvn', '5rvq', '7d6h', '6wcf', '5rvj', '5rvr', '7kr0', '7bf4', '7ofu']
-print(len(doit))
-
 print("iterate over entries...")
 # iterate over all entries in dir
 for entry in os.listdir():
-    if os.path.isdir(join(os.getcwd(), entry)) and doit.count(entry) > 0:
+    if os.path.isdir(join(os.getcwd(), entry)):
+        print(entry)
         entry_pdb_path = join(os.getcwd(), entry, entry + ".pdb")
-        print(entry_pdb_path)
         # load model from pdb
-        model = gemmi.read_pdb(entry_pdb_path)[0]
+        try:
+            pdb_entry = gemmi.read_pdb(entry_pdb_path)
+        except:
+            entry_pdb_path = join(os.getcwd(), entry, entry + "_false.pdb")
+            pdb_entry = gemmi.read_pdb(entry_pdb_path)
+        model = pdb_entry[0]
         # for each chain, load sequence if polypeptide
         best_chain_score = -1000
         best_chain_domain = "None"
-        print(len(model))
         for x in range(len(model)):
-            print(model[x].whole().check_polymer_type())
-            print()
-            if model[x].whole().check_polymer_type() == gemmi.PolymerType.PeptideL:
-                chain_sequence = model[x].whole().make_one_letter_sequence()
+            if (model[x].whole().check_polymer_type() == gemmi.PolymerType.PeptideL
+                or model[x].whole().check_polymer_type() == gemmi.PolymerType.Unknown):
+                chain_sequence = model[x].whole().make_one_letter_sequence().replace('-', '')
                 # do sequence alignment for each sequence from fasta and track best
                 best_score = -1000
                 best_domain = "None"
@@ -84,15 +91,50 @@ for entry in os.listdir():
                     best_chain_domain = best_domain
         #print(best_chain_domain + " " + str(best_chain_score))
         # check if domain was already added to final results
+        save_entry = [entry, pdb_entry.resolution, 0, 0, 0]
+        for key, value in pdb_entry.info.items():
+            if key == "_struct.title":
+                save_entry[2] = value
+            if key == "_exptl.method":
+                save_entry[3] = value
+            if key == "_pdbx_database_status.recvd_initial_deposition_date":
+                save_entry[4] = value
+        
         domain_added = False
         for result_list in entries_and_domains:
             if result_list[0] == best_chain_domain:
                 domain_added = True
-                result_list[1].append(entry)
+                result_list[1].append(save_entry)
                 break
         if not domain_added:
-            entries_and_domains.append([best_chain_domain, [entry]])
+            entries_and_domains.append([best_chain_domain, [save_entry]])
         
-print(entries_and_domains)
+# print out results
+total = 0
 for result_list in entries_and_domains:
-    print(result_list[0] + " " + str(len(result_list[1])))
+    print(result_list[0] + ": " + str(len(result_list[1])))
+    total += len(result_list[1])
+print("total: " + str(total))
+
+# write excel
+workbook_path = join(os.getcwd(), "domains_and_infos_of_" + args.protein + '.xlsx')
+workbook = xlsxwriter.Workbook(workbook_path)
+print(workbook_path)
+
+for result_list in entries_and_domains:
+    worksheet = workbook.add_worksheet(result_list[0])
+    worksheet.write(0, 0, "PDB")
+    worksheet.write(0, 1, "resolution")
+    worksheet.write(0, 2, "title")
+    worksheet.set_column('C:C', 110)
+    worksheet.write(0, 3, "method")
+    worksheet.set_column('D:D', 20)
+    worksheet.write(0, 4, "date")
+    worksheet.set_column('E:E', 12)
+    row = 1
+    for entry in result_list[1]:
+        for x in range(5):
+            worksheet.write(row, x, entry[x])
+        row += 1
+
+workbook.close()
